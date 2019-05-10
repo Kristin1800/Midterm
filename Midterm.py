8f403c83fec2d015e60d2d4355c1ab1c7466e7af
Kristin1800 <47115526+Kristin1800@users.noreply.github.com>
2019-05-10 10:12:05 -0400

    Update Midterm.py

--- Midterm.py
+++ Midterm.py
@@ -2,249 +2,232 @@
 # Midterm - 3/20/19
 import re
 import sys
-import string
 
-spr = "*****************************************"
+# Opening the file
+
 file = open('ALevel.java', mode='r')
-readCode = file.read()
-
-# finding the class statements and then removing them with regex
-br = re.findall(
-r'class\s+[A-Za-z]+({)\s*[\w\s(\[\]\)\{\}\/=\.;"?+<>,*]+(\})(\s+class\s+[A-Za-z]+({)\s*[\w\s(\[\]\)\{\}\/=\.;"?+<>,*]+(\}))*',
-    readCode)
-
-if not br:
-    print("Something went wrong, please make sure your class brackets are formatted correctly.")
-
-Cclass = re.compile(r'class\s+[\w]*{')
-Bclass = re.compile(r'class\s+[A-Za-z]+{\n')
+Acode = file.read()
 
 
+braces = re.findall(
+    r'class\s+[A-Za-z]+({)\s*[\w\s(\[\]\)\{\}\/=\.;"?+<>,*]+(\})(\s+class\s+[A-Za-z]+({)\s*[\w\s(\[\]\)\{\}\/=\.;"?+<>,*]+(\}))*',
+    Acode)
 
-if not Bclass.search(readCode) and Cclass.search(readCode):
-    print("Something went wrong, check your class declaration.")
+if not braces:
+    print("Your class braces are formatted incorrectly")
+
+classverif = re.compile(r'class\s+[A-Za-z]+{\n')  
+classcheck = re.compile(r'class\s+[\w]*{')
+
+if not classverif.search(Acode) and classcheck.search(Acode):
+    print("You have an error regarding your class declaration")
     sys.exit();
 
-if Bclass.search(readCode):
-    noClass = Bclass.sub("", readCode)
+if classverif.search(Acode):
+    noclass = classverif.sub("", Acode)
 
 else:
-    print("Something went wrong, check your class declaration.")
+    print("You have an error regarding your class declaration")
     sys.exit();
-print(spr)
 
-# comments regex
-comm = re.compile(r'(\/\*\s+[a-zA-Z\s\-\,\.]*\*\/)')
-# one line comment regex
-oneComm = re.compile(r'(\/{2}[\w\s\!]*$)', re.MULTILINE)
-# multiline comments
-if comm.search(noClass):
-# without comments
-    woComm = comm.sub(" ", noClass, count=2)
-# single line comments
-if oneComm.search(woComm):
-    noComm = oneComm.sub("", woComm, count=2)
-print(spr)
+comments = re.compile(r'(\/\*\s+[a-zA-Z\s\-\,\.]*\*\/)')
+onelinecomment = re.compile(r'(\/{2}[\w\s\!]*$)', re.MULTILINE)
 
-#Getting rid of spaces
+if comments.search(noclass):  
+    withoutcomments = comments.sub(" ", noclass, count=2) 
+
+if onelinecomment.search(withoutcomments):  # single line comments
+    nocomments = onelinecomment.sub("", withoutcomments, count=2)
+
+
 nospace = re.compile(r'(\s{2,})')
 
-if nospace.search(woComm, oneComm):
-    oneComm = nospace.sub("",woComm)
+if nospace.search(nocomments):
+    oneline = nospace.sub("", nocomments)
 
 
+importstate = re.compile(r'import\s+java\.(io.\*|util.Scanner|text.\*|awt.\*|util.regex.\*);', re.MULTILINE)
+importcheck = re.compile(r'import\s+[\w.*]*;?')
 
-# using regex to find and get rid of the imports
-impoState = re.compile(r'import\s+java\.(io.\*|util.Scanner|text.\*|awt.\*|util.regex.\*);', re.MULTILINE)
-
-impoCheck = re.compile(r'import\s+[\w.*]*;?')
-
-if not impoState.search(oneComm) and impoCheck.search(oneComm):
-    print("Something went wrong, check your import statements.")
-    print(impoCheck.findall(oneComm))
+if not importstate.search(oneline) and importcheck.search(oneline):
+    print("You have an error regarding your import statement")
+    print(importcheck.findall(oneline))
     sys.exit()
 
-if impoState.search(oneComm):
-    noImpo = impoState.sub("", oneComm)
+if importstate.search(oneline):
+    noimport = importstate.sub("", oneline)
 
-print(spr)
+# print(noimport)
 
-# Using regex to find and remove the last bracket
-publicStatic = re.compile(r'public\s+static\s+void\s+main\s+(\(String\s+\[\]\s+arg(s|ss)\)\{)')
-check = re.compile(r'public\s*sta(\w+)\s*\w*\s*\w*\s*[\(\w\s\[\]\)]*\{*')
 
-if not publicStatic.search(noImport) and check.search(noImport):
-    print("Something went wrong, check your public static statements.")
+
+publicstatic = re.compile(r'public\s+static\s+void\s+main\s+(\(String\s+\[\]\s+arg(s|ss)\)\{)')
+check0 = re.compile(r'public\s*sta(\w+)\s*\w*\s*\w*\s*[\(\w\s\[\]\)]*\{*')
+
+if not publicstatic.search(noimport) and check0.search(noimport):
+    print("You have an error regarding your public static statement")
     sys.exit()
 
-if publicStatic.search(noImport):
-    nopublic = publicStatic.sub("", noImport)
+if publicstatic.search(noimport):
+    nopublic = publicstatic.sub("", noimport)
 
-print(spr)
+# print (nopublic) #->feel free to check the progress by uncommenting
 
-# Finding and removing string declarations
 string = re.compile(r'(public\s+|private\s+)*String\s+([A-Za-z]*)(\s*\=\s*(\"([A-Za-z\s+])*\"|sc\.next\(\)))*;')
 check1 = re.compile(r'(public\s+|private\s+)*St\w+\s*\w+[\w=\s".()]*;?')
 
 if not string.search(nopublic) and check1.search(nopublic):
-    print("Something went wrong, check your strings.")
+    print("You have an error regarding on of your Strings")
     sys.exit()
 
 if string.search(nopublic):
     nostrings = string.sub("", nopublic)
 
+# print(nostrings)
 
-print(spr)
 
-# Finding and removing variables
-numregex = re.compile(
-    r'(public\s+|private\s+)*int\s+[\w]*(;|\s*\=[\s.\w\(\)]*;)')
-numcheck = re.compile(r'(public\s+|private\s+)*(?<![rlo\s])i\w+\s+\w+(?![p,])\s*(;|\=?\s*\w+.\w+\(?\)?;|\=\s+\d;)*')
+intregex = re.compile(
+    r'(public\s+|private\s+)*int\s+[\w]*(;|\s*\=[\s.\w\(\)]*;)')  # is able to detect int age; || int age = 56;
+intcheck = re.compile(r'(public\s+|private\s+)*(?<![rlo\s])i\w+\s+\w+(?![p,])\s*(;|\=?\s*\w+.\w+\(?\)?;|\=\s+\d;)*')
 
-boolregex = re.compile(r'(public\s+|private\s+)*boolean\s+\w+\s*(;|=\s?(false|true);)')
-boolcheck = re.compile(r'(public\s+|private\s+)*bo\w+\s+\w([=\s\w])*;?')
+booleanregex = re.compile(r'(public\s+|private\s+)*boolean\s+\w+\s*(;|=\s?(false|true);)')
+booleancheck = re.compile(r'(public\s+|private\s+)*bo\w+\s+\w([=\s\w])*;?')
 
-if numcheck.search(nostrings) and not numregex.search(nostrings):
-    print("Something went wrong, check your int declaration statements")
+if intcheck.search(nostrings) and not intregex.search(nostrings):
+    print("You have an error regarding one of your int declaration statements")
     sys.exit()
 else:
-    nonum = numregex.sub("", nostrings)
+    noints = intregex.sub("", nostrings)
 
-if not boolregex.search(nonums) and boolcheck.search(nonums):
-    print("Something went wrong, check your boolean declaration statements")
+if not booleanregex.search(noints) and booleancheck.search(noints):
+    print("You have an error regarding one of your boolean declaration statements")
     sys.exit()
 
-if boolregex.search(nonums):
-    nobool = boolregex.sub("", nonums)
+if booleanregex.search(noints):
+    nobool = booleanregex.sub("", noints)
+
+# print(nobool)
 
 
-print(spr)
-
-# Finding and removing any system.out.prints
-sysregex = re.compile(
+outregex = re.compile(
     r'System.out.print(ln)?\((\"[\w\s?<+>=.]+\"(\s*\+\s*\w+)*(\s*\+\s*\"\s*[\w\s]+\")?)?(\s*\+\s*\w+)?(\w\+\w)?(\s*\+\s*\"\s*[\w\s?<+>=.]+\")?\);')
 check2 = re.compile(
     r'Sy\w+\.\w+\.?\w+\(?\"?[\w\s?.<>=+]+\"?\s*\+*\s*[\w\s]*\+*\s*\"*[\w\s?.<>=+]*\"*\s*\+*\s*[\w\s]*\+*\s*\"*[\w\s?.<>=+]*\"*\)?;?')
 
-if check2.search(nobool) and not sysregex.search(nobool):
-    print("Something went wrong, check your System output statements")
+if check2.search(nobool) and not outregex.search(nobool):
+    print("You have an error regarding one of your System output statements")
     sys.exit()
 
-if sysregex.search(nobool):
-    nosys = sysregex.sub("", nobool)
+if outregex.search(nobool):
+    noout = outregex.sub("", nobool)
 
-print(spr)
+# print (noout)
 
 
-# Finding and removing assignment statements
 scan = re.compile(r'Scanner\s+(\w)+\s*\=\s*new\s+Scanner\(System\.in\);')
 scancheck = re.compile(r'Sc\w+\s+\w+\s*[=\s\w]+\(?\w*\.?\w*\)?;?')
 assign = re.compile(r'\w+\s*=\s*[\w\s\+\-\/\*\%\.\(\)]*;')
 
-if not scan.search(nosys) and scancheck.search(nosys):
-    print("Something went wrong, check your Scanner declarations")
+if not scan.search(noout) and scancheck.search(noout):
+    print("There is an error regarding one of your Scanner declarations")
     sys.exit()
 
-if scan.search(nosys):
-    noscan = scan.sub("", nosys)
+if scan.search(noout):
+    noscan = scan.sub("", noout)
 
 if assign.search(noscan):
     noassign = assign.sub("", noscan)
 
+# print(noassign)
 
-print(spr)
 
-# Finding and removing while statements
-whregex = re.compile(r'while(\([A-Za-z]+\s*[<=>!]+\s*[0-9A-Za-z]+\)){([A-Za-z.()"\s+-\/?;]*)}')
+whileregex = re.compile(r'while(\([A-Za-z]+\s*[<=>!]+\s*[0-9A-Za-z]+\)){([A-Za-z.()"\s+-\/?;]*)}')
 check3 = re.compile(r'wh\w+\s*\(?[\w\s<=>\/]+\)?{?[A-Za-z.()"\s+-\/?;]?}?')
 
-if not whregex.search(noassign) and check3.search(noassign):
-    print("Something went wrong, check your while loop declarations")
+if not whileregex.search(noassign) and check3.search(noassign):
+    print("There is an error regarding one of your while loop declarations")
     sys.exit()
 
-if whregex.search(noassign):
-    nowh = whregex.sub("", noassign)
+if whileregex.search(noassign):
+    nowhile = whileregex.sub("", noassign)
+
+# print(nowhile)
 
 
-print(spr)
-
-# Finding and removing if statements
 ifregex = re.compile(
     r'if(\s*\([A-Za-z0-9\s<>=!-+)]*){([A-Za-z.()"<>+-+!0-9;=]*)}(else if{[A-Za-z.()"<>+-+!0-9;=]*})*(else{[A-Za-z.()"<>+-+!0-9;=]*})*')
 check4 = re.compile(
     r'if\s*\(?\s*[\w+=><\/\s]+\)?\{[\w\s<>=!-+)]*\}?\s*(else if\s*\(?\s*[\w+=><\/]+\)?\{[\w\s<>=!-+)]*\})*(else\s*\{?[\w\s<>=!-+)]*\}?)*')
 
-if not ifregex.search(nowh) and check4.search(nowh):
-    print("Something went wrong, check your if statements")
+if not ifregex.search(nowhile) and check4.search(nowhile):
+    print("There is an error regarding one of your if(else) statements")
     sys.exit()
 
+if ifregex.search(nowhile):
+    noif = ifregex.sub("", nowhile)
 
-if ifregex.search(nowh):
-    noif = ifregex.sub("", nowh)
+# print(noif)
 
-print(spr)
 
-# Finding and removing object declaration statements
-objregex = re.compile(r'Person\s+\w+\s*\=\s*new\s+Person\(\w+,\s+\w+\);')
-objcheck = re.compile(r'P\w+\s*\w+\s*\=?\s*[\w\s(),]+;')
+objectify = re.compile(r'Person\s+\w+\s*\=\s*new\s+Person\(\w+,\s+\w+\);')
+objectcheck = re.compile(r'P\w+\s*\w+\s*\=?\s*[\w\s(),]+;')
 
-if not objregex.search(noif) and objcheck.search(noif):
-    print("Something went wrong, check your object declaration statements")
+if not objectify.search(noif) and objectcheck.search(noif):
+    print("There is an error regarding one of your object declaration statements")
     sys.exit()
 
-if objregex.search(noif):
-    noobj = objregex.sub("", noif)
+if objectify.search(noif):
+    noobject = objectify.sub("", noif)
 
-print(spr)
+# print(noobject)
 
-# Finding and removing person info
-pregex = re.compile(r'you\.printInfo\(\);')
-pcheck = re.compile(r'\w+\.\w+\(?\)?;?')
 
-if not pregex.search(noobject) and pcheck.search(noobj):
-    print("Something went wrong, check your method statements.")
+method = re.compile(r'you\.printInfo\(\);')
+methodcheck = re.compile(r'\w+\.\w+\(?\)?;?')
+
+if not method.search(noobject) and methodcheck.search(noobject):
+    print("There is an error regarding one of your method calling statements")
     sys.exit()
 
-if pregex.search(noobj):
-    nop = pregex.sub("", noobj)
+if method.search(noobject):
+    nomethod = method.sub("", noobject)
 
-print(spr)
+# print(nomethod)
+
 
 obclass = re.compile(
     r'(public|private)\s+\w+\((int|String|double)\s+\w(,\s+(int|String|double)\s+\w)*(,\s+(int|String|double)\s+\w)*\){}')
 obcheck = re.compile(r'(public|private)*\s*\w*\s*\(?[\w\s,]+\)?{}')
 
-if not obclass.search(nop) and obcheck.search(nop):
-    print("Something went wrong, check your object class declaration statements")
+if not obclass.search(nomethod) and obcheck.search(nomethod):
+    print("There is an error regarding one of your object class declaration statements")
     sys.exit()
 
-if obclass.search(nop):
-    noob = obclass.sub("", nop)
+if obclass.search(nomethod):
+    noob = obclass.sub("", nomethod)
+
+# print(noob)
 
 
-print(spr)
-
-# Finding and removing the object method declaration statements
 opening = re.compile(r'(public|private)\s+(static\s+)*void\s+\w+\([\w,\s]*\){')
 opencheck = re.compile(r'(public|private)*\s*\w+\s*\w+\s*\w*\(?[\w\D\s]*\)?{')
 
 if not opening.search(noob) and opencheck.search(noob):
-    print("Something went wrong, check your method declaration statements")
+    print("There is an error regarding one of your method declaration statements")
     sys.exit()
 
 if opening.search(noob):
     noopen = opening.sub("", noob)
 
+# print(noopen)
 
-print(spr)
+# Final check
+final = re.compile(r'\w')
 
-# Compiker
-test = re.compile(r'\w')
-
-if test.search(noopen):
-    print("Something went wrong, check your code again.")
+if final.search(noopen):
+    print("There's an error in your code the compiler missed")
     sys.exit()
 
 file.close()
 
-print("Your code was successfully compiled! No errors detected.")
+print("Your code was compiled with no errors")
